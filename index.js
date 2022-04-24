02d4928d9b7f3277a13815f571c07b7622d7e06a
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-24 19:48:12 -0300

    add middlewares to prevent errors

--- index.js
+++ index.js
@@ -11,24 +11,49 @@ server.use(express.json());
 
 const cursos = ["Node JS", "JavaScript", "React Native"];
 
+// middleware global
+server.use((req, res, next) => {
+  console.log(`url chamada: ${req.url}`);
+
+  return next();
+});
+
+function checkCurso(req, res, next) {
+  if (!req.body.name) {
+    return res.status(404).json({ error: "Insira um nome para o curso" });
+  }
+
+  return next();
+}
+
+function checkIndexCurso(req, res, next) {
+  const curso = cursos[req.params.index];
+  
+  if (!curso) {
+    return res.status(404).json({ error: "O curso não existe" });
+  }
+
+  return next();
+}
+
 server.get("/cursos", (req, res) => {
   return res.json(cursos);
 });
 
-server.get("/cursos/:index", (req, res) => {
+server.get("/cursos/:index", checkIndexCurso, (req, res) => {
   const { index } = req.params;
 
   return res.json(cursos[index]);
 });
 
-server.post("/cursos", (req, res) => {
+server.post("/cursos", checkCurso, (req, res) => {
   const { name } = req.body;
   cursos.push(name);
 
   return res.json(cursos);
 });
 
-server.put("/cursos/:index", (req, res) => {
+server.put("/cursos/:index", checkCurso, checkIndexCurso, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -37,7 +62,7 @@ server.put("/cursos/:index", (req, res) => {
   return res.json(cursos);
 });
 
-server.delete("/cursos/:index", (req, res) => {
+server.delete("/cursos/:index", checkIndexCurso, (req, res) => {
   const { index } = req.params;
 
   cursos.splice(index, 1);
